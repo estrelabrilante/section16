40ae234a6cf09b9b25f5f2928b8945d07ba94f41
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-02-15 06:49:39 +0530

    initial commit

---
+++ src/pages/coaches/CoachDetails.vue
@@ -0,0 +1,6 @@
+<template>
+  Details of coach
+  <!-- Child component:ContactCoach router-view in parent component: CoachDetails -->
+  <router-view></router-view>
+  <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>Filter coach based on experience</section>
+  <section>
+    List of coaches
+    <ul>
+      <li>
+        <button>Refresh</button>
+      </li>
+      <li>
+        <router-link to="/register">Register</router-link>
+      </li>
+    </ul>
+  </section>
+</template>
